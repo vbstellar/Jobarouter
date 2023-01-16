7123fbc81adce43742f4072d518633db769e5525
vishu <bansalvishu56@gmail.com>
2023-01-16 15:45:33 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
+//Components
+import Home from "./Components/Home";
+import About from "./Components/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>JobaRouter</h1>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="about">ABOUT US</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are a firm runned by a few college friends who met and started to do
+        fun all the time. But later in our career we decided to open this firm
+        together and work as a team on it. This is a business that we build with
+        our own hands.
+      </p>
+    </div>
+  );
+}
