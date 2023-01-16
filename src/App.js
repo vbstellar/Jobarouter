efae3b19b3c943d9b396ae1a3f5612a0eedf68a5
vishu <bansalvishu56@gmail.com>
2023-01-16 18:17:52 +0530

    route 6.4v added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 //Components
 import Home from "./Components/Home";
 import About from "./Components/About";
 
+//Layout
+import RootLayout from "./Layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>JobaRouter</h1>
-          <NavLink to="/">HOME</NavLink>
-          <NavLink to="about">ABOUT US</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/Layouts/RootLayout.js
@@ -0,0 +1,19 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>JobaRouter</h1>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="about">ABOUT US</NavLink>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
